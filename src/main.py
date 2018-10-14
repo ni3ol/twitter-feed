24e074064e20112ff4371bee10231e94d5c0df50
Nicol <nvojacek@gmail.com>
2018-10-14 14:46:45 +0200

    Tests and working simulation

--- src/main.py
+++ src/main.py
@@ -1,32 +1,111 @@
-user_followers_mapping = {
-    'Ward': ['Martin', 'Alan'],
-    'Alan': ['Martin'],
- }
+import sys
 
-tweets = [
-    ('Alan', 'If you have a procedure with 10 parameters, you probably missed some.'),
-    ('Ward', 'There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.'),
-    ('Alan', 'Random numbers should not be generated with a method chosen at random.'),
-]
+def format_user_followers_entry(entry):
+    '''Takes a user followers entry and formats it into a tuple of user and the users they follow.
+    '''
+    entry = entry.rstrip()
+    entry_without_commas = entry.replace(',', '')
+    entry_without_follows = entry_without_commas.replace(' follows', '')
+    user_followers_list = entry_without_follows.split(' ')
+    user = user_followers_list.pop(0)
+    followers_list = user_followers_list
+    return user, followers_list
 
-def get_users_from_user_followers_mapping(user_followers_mapping):
-    users = []
-    for user, followers in user_followers_mapping.items():
-        if user not in users:
-            users.append(user)
-        for follower in followers:
-            if follower not in users:
-                users.append(follower)
+def get_new_users(users, user_followers):
+    '''Get new users.
+    '''
+    new_users = set()
+    user = user_followers[0]
+    followers = user_followers[1]
+    if user not in users:
+        new_users.add(user)
+    for follower in followers:
+        if follower not in users:
+            new_users.add(follower)
+    return new_users
+
+def update_user_followers_mapping(user_followers_mapping, user_followers):
+    '''Return user and updated set of users they follow.
+    '''
+    user = user_followers[0]
+    followers = user_followers[1]
+    if user in user_followers_mapping:
+        user_followers_mapping[user].update(followers)
+        new_followers = user_followers_mapping[user]
+    else:
+        new_followers = set(followers)
+    return user, new_followers
+
+def get_users(user_file):
+    '''Get complete list of all users.
+    '''
+    users = set()
+    file = open(user_file, 'r')
+    for entry in file:
+        entry = format_user_followers_entry(entry)
+        new_users = get_new_users(users, entry)
+        users.update(new_users)
     return users
 
-def get_users_tweets(user, user_followers_mapping, tweets):
-    followers = user_followers_mapping[user]
-    user_tweets = []
-    for tweet in tweets:
-        author = tweet[0]
-        if author in followers or author == user:
-            user_tweets.append(tweet)
-    return user_tweets
+def get_user_followers_mapping(user_file):
+    '''Get mapping of users to the users they follow.
+    '''
+    user_followers_mapping = {}
+    file = open(user_file, 'r')
+    for entry in file:
+        entry = format_user_followers_entry(entry)
+        user_followers = update_user_followers_mapping(user_followers_mapping, entry)
+        user_followers_mapping[user_followers[0]] = user_followers[1]
+    return user_followers_mapping
+
+def format_tweet_entry(entry):
+    '''Takes a tweet and formats it into a tuple containing a user and their followers.
+    '''
+    user = entry.split(' ', 1)[0]
+    user = user.replace('>', '')
+    tweet = entry.split(' ', 1)[1]
+    tweet = tweet.rstrip()
+    return user, tweet
 
 def format_tweet(tweet):
+    '''Format tweet.
+    '''
     return '@{}: {}'.format(tweet[0], tweet[1])
+
+def get_tweets(tweet_file):
+    '''Get user and tweet and output as tuple.
+    '''
+    tweets = []
+    file = open(tweet_file, 'r')
+    for entry in file:
+        formatted_entry = format_tweet_entry(entry)
+        tweets.append(formatted_entry)
+    return tweets
+
+def get_simulation(users, user_followers_mapping, tweets):
+    '''Return ordered users and their tweets.
+    '''
+    simulation = []
+    for user in sorted(users):
+        simulation.append(user)
+        try:
+            followers = user_followers_mapping[user]
+        except:
+            followers = set()
+        for tweet in tweets:
+            if tweet[0] == user or tweet[0] in followers:
+                simulation.append(format_tweet(tweet))
+    return simulation
+
+if __name__ == '__main__':
+    try:
+        user_file = sys.argv[1]
+        tweet_file = sys.argv[2]
+    except IOError:
+        print('Wrong number of arguments.')
+    users = get_users(user_file)
+    user_followers_mapping = get_user_followers_mapping(user_file)
+    tweets = get_tweets(tweet_file)
+    simulation = get_simulation(users, user_followers_mapping, tweets)
+    for entry in simulation:
+        print(entry)
---
+++ tests/test_simulation.py
@@ -0,0 +1,67 @@
+import pytest
+import unittest 
+from src.main import format_user_followers_entry, get_new_users, update_user_followers_mapping, get_users, get_user_followers_mapping, format_tweet_entry, format_tweet, get_tweets, get_simulation
+
+class TestSimulation(unittest.TestCase):
+    def test_format_tweet_formats_tweet(self):
+        result = format_tweet(('Alan', 'Hello.'))
+        expected_result = '@Alan: Hello.'
+        assert result == expected_result
+
+    def test_format_user_followers_entry_returns_user_followers_list(self):
+        entry = 'Ward follows Martin, Alan'
+        result = format_user_followers_entry(entry)
+        expected_result = ('Ward', ['Martin', 'Alan'])
+        assert result == expected_result
+
+    def test_update_empty_user_followers_mapping_returns_user_and_their_users_followed(self):
+        user_followers = ('Ward', ['Martin', 'Alan'])
+        result = update_user_followers_mapping({}, user_followers)
+        expected_result = ('Ward', {'Martin', 'Alan'})
+        assert result == expected_result
+
+    def test_update_user_followers_mapping_returns_user_and_updated_users_followed(self):
+        user_followers = ('Ward', ['Martin', 'Alan'])
+        result = update_user_followers_mapping({'Ward': {'Martin'}}, user_followers)
+        expected_result = ('Ward', {'Martin', 'Alan'})
+        assert result == expected_result
+
+    def test_format_tweet_entry_returns_user_and_tweet_tuple(self):
+        entry = 'Alan> Hello.\n'
+        result = format_tweet_entry(entry)
+        expected_result = ('Alan', 'Hello.')
+        assert result == expected_result
+
+    def test_get_new_users_returns_new_users_obtained_from_entry(self):
+        user_followers = ('Ward', ['Martin', 'Alan'])
+        result = get_new_users(set(), user_followers)
+        expected_result = {'Ward', 'Martin', 'Alan'}
+        assert result == expected_result
+
+    def test_get_users_returns_users_obtained_from_user_file(self):
+        result = get_users('./data/user.txt')
+        expected_result = {'Alan', 'Martin', 'Ward'}
+        assert result == expected_result
+
+    def test_get_user_followers_mapping_returns_mapping_of_users_to_followers_from_user_file(self):
+        result = get_user_followers_mapping('./data/user.txt')
+        expected_result = {'Alan': {'Martin'}, 'Ward': {'Alan', 'Martin'}}
+        assert result == expected_result
+
+    def test_get_tweets_returns_list_of_user_tweet_entries(self):
+        result = get_tweets('./data/tweet.txt')
+        expected_result = [
+            ('Alan', 'If you have a procedure with 10 parameters, you probably missed some.'),
+            ('Ward', 'There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.'),
+            ('Alan', 'Random numbers should not be generated with a method chosen at random.'),
+        ]
+        assert result == expected_result
+
+    def test_simulation_returns_simulation_of_users_and_tweets_as_list(self):
+        users = {'Ward', 'Martin', 'Alan'}
+        user_followers_mapping = {'Ward': {'Martin', 'Alan'}}
+        tweets = [('Ward', 'Random numbers should not be generated with a method chosen at random.')]
+        result = get_simulation(users, user_followers_mapping, tweets)
+        expected_result = ['Alan', 'Martin', 'Ward', '@Ward: Random numbers should not be generated with a method chosen at random.'] 
+        assert result == expected_result
+
